7b12df3cad688cb4a5db11c90887eb9b1b815946
Abrham2119 <abrhamzewdu2119.com>
2024-01-11 00:10:33 +0300

    changed the popularproduct file

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -8,8 +8,8 @@ const PopularProducts = () => {
         <p className=' lg:max-w-lg mt-2 font-montserrat text-slate-gray'>Experience top-notch quality and style with our sought-after
           selections. Discover a world of comfort, design, and value</p>
       </div>
-      <div className=' mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-14 sm:gap-4'> 
-      
+      <div className=' mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-14 sm:gap-4'>           
+
 
       </div>
 
